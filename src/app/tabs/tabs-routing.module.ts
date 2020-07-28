86539928ca368a1728f6a64347ee64e1f2404526
Hendricks-3D <odainbrown111@gmail.com>
2020-07-27 21:57:08 -0500

    tabs modified... white theme added

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -20,6 +20,10 @@ const routes: Routes = [
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
+        path: 'tips',
+        loadChildren: () => import('../Pages/plant-tips/plant-tips.module').then(m => m.PlantTipsPageModule)
+      },
+      {
         path: '',
         redirectTo: '/tabs/tab1',
         pathMatch: 'full'
